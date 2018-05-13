f74ad57f4cb2dbfc3f0cf4b3b95243c4c8e12022
James Stillman <james.stillman@deakin.edu.au>
2018-05-13 13:10:50 +1000

    added messaging component service

--- angular-weather/src/app/services/weather.service.ts
+++ angular-weather/src/app/services/weather.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 import { WeatherStation } from '../models/weather-station';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +13,13 @@ weatherData = [
     { stationName: 'Cape Otway', direction: 'NW', speed:4, maxSpeed:19 },
     { stationName: 'Port Fairy', direction: 'SW', speed:8, maxSpeed:10 },
 ];
-  constructor() { }
+
+constructor(private messageService: MessageService) { }
 
     getWeatherData(): Observable<WeatherStation[]> {
+        // TODO: send the message _after_ fetching the heroes
+        this.messageService.add('HeroService: fetched weatherdata');
+
         return of(this.weatherData);
     }
 
